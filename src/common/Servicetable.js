e1ffe475dd72cedddc265222e77c8efcd3fcc0bb
Shubham-Gehlod <shubham.gahlod@samyotech.com>
2025-03-06 18:35:38 +0530

    Delete Dummy service table

--- src/common/Servicetable.js
+++
@@ -1,8 +0,0 @@
-const serviceTables = [
-  { label: 'Table 1', status: 'Available' },
-  { label: 'Table 2', status: 'Occupied' },
-  { label: 'Table 3', status: 'Available' },
-  { label: 'Table 4', status: 'Reserved' }
-];
-
-export default serviceTables;
