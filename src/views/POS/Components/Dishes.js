556128c5b5dbd403c3fb3ffafe09bd79a583331f
Shubham-Gehlod <shubham.gahlod@samyotech.com>
2025-03-22 17:51:50 +0530

    Category and Subcategory in POS

--- src/views/POS/Components/Dishes.js
+++ src/views/POS/Components/Dishes.js
@@ -2,26 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { getApi } from 'core/apis/apiClient.js';
 import { urls } from 'core/constant/urls';
 
-const useDishes = () => {
-  const [dishes, setRows] = useState([]);
+const useDishes = (categories) => {
+const [dishes, setRows] = useState([]);
+
   const fetchData = async () => {
     const response = await getApi(urls?.item?.get);
 
-    const formattedData = response?.data?.map((item, index) => ({
-      id: item?._id,
-      name: item?.name,
+    
+    const formattedData = response?.data
+      ?.filter((item) =>
+        categories.some(
+          (category) =>
+            category.checked && 
+            category.items.some(
+              (catItem) => catItem.checked && catItem._id === item._id 
+            )
+        )
+      )
+      .map((item) => ({
+        id: item?._id,
+        name: item?.name,
+        image: item?.itemImage,
+        price: item?.price,
+        cost: item?.cost
+      }));
 
-      image: item?.itemImage,
-
-      price: item?.price,
-      cost: item?.cost
-    }));
     setRows(formattedData);
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [categories]); 
+
   return dishes;
 };
 
