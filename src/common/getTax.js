f6d927667a8d850c724b2ffb1a5a79c3f74344cf
Shubham9Gehlod <shubham.gehlod@samyotech.in>
2025-04-11 18:33:05 +0530

    updates

--- src/common/getTax.js
+++ src/common/getTax.js
@@ -10,12 +10,9 @@ const useGetTax = () => {
     const fetchData = async () => {
       const UserInfo = getUserInfoFromToken();
       const id = UserInfo?.id;
-      console.log("id", id);
-
       try {
         const response = await getApi(urls?.employee?.getbyid.replace(':id', id));
         const tax = response?.data?.tax;
-        console.log("tax", tax);
         setTaxPercentage(tax);
       } catch (error) {
         console.error("Error fetching tax:", error);
